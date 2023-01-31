ac1df98cb631153a1dbb0c7c3add78fd7dc8d30a
GuiBatalhoti <guibatalhoti@gmail.com>
2023-01-31 17:51:05 -0300

    Mudando o nome do diretório do código

--- Sistema-agendamento-clinico/src/User/Administrador.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-import java.io.Serializable;
-
-public class Administrador extends Usuario implements Serializable{
-    public Administrador(String nomeDeUsuario, char[] senha, String CPF) {
-        super(nomeDeUsuario, senha, CPF);
-    }
-}
